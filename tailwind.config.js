a7e63beaf347b165468ddd690d3833dbf9644acc
Putra Michael Sitohang <ptramichsthg@gmail.com>
2026-03-10 20:10:37 +0700

    fix(tailwind): add darkMode class to enable dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // /Users/haimac/Project/clipper-ai-fe/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
